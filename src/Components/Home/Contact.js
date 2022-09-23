0101c9992ebdb56f98e012b40689673da89698d8
Parvez Miah <96688877+Parvez-Rahman100@users.noreply.github.com>
2022-09-23 18:51:59 +0600

    contact updated

--- src/Components/Home/Contact.js
+++ src/Components/Home/Contact.js
@@ -1,58 +1,54 @@
 import React from 'react';
-import { Icon } from '@iconify/react';
+import { Icon, InlineIcon } from '@iconify/react';
 
 
 const Contact = () => {
-    return (
-        <div className=' my-10'>
-            <div id='contact' className="hero min-h-screen bg-base-100">
-    <div className="grid sm:grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-4">
-    <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold text-green-600">Contact Us</h1>
-      <p className="py-4">Always available for work if the right project comes along, Feel free to contact us!</p>
-      
-      <h4><Icon icon="ic:baseline-alternate-email" /><p className='text-green-600 font-bold'>iamparvezrahman@gmail.com</p></h4>
-      <h4><Icon icon="bxs:phone-call" /> <span className='text-green-600 font-bold'>+8801686172299</span></h4>
-      <h4><Icon icon="akar-icons:location" /><span className='text-green-600 font-bold'>16 Indira Road, Dhaka-1215</span></h4>
-      <div className='flex lg:justify-start justify-center items-center my-4'>
-        <a target='_blank' rel="noreferrer" href='https://github.com/Parvez-Rahman100'><Icon className=' mx-7 w-10  h-6 mt-5' icon="fa:github" /></a>
-        <a target='_blank' rel="noreferrer" href='https://www.linkedin.com/in/parvez-miah-945910229/'><Icon className=' mx-7 w-10  h-6 mt-5' icon="line-md:linkedin" /></a>
-        <a target='_blank' rel="noreferrer" href='https://www.facebook.com/groups/3128511790748537'><Icon className=' mx-7 w-10  h-6 mt-5' icon="akar-icons:facebook-fill" /></a>
-        <a target='_blank' rel="noreferrer" href='https://www.instagram.com/i_amparvezrahman/'><Icon className=' mx-7 w-10  h-6 mt-5' icon="akar-icons:instagram-fill" /></a>
+  return (
+    <div id='contact' className="hero min-h-screen bg-base-100">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold text-green-600">Contact Us</h1>
+          <p className="py-4">Always available for work if the right project comes along, Feel free to contact us!</p>
+
+          <h4><InlineIcon className=' mt-4' style={{ fontSize: '30px' }} icon="entypo:email" /> <span className='text-green-600 font-bold'>contact@edumall.com</span></h4>
+          <h4><InlineIcon className=' mt-4' style={{ fontSize: '30px' }} icon="bxs:phone-call" /> <span className='text-green-600 font-bold'>(+88)-1990-6886</span></h4>
+          <div className='flex lg:justify-start justify-center items-center my-4'>
+            <a className=' mx-5 mt-3' target='_blank' rel="noreferrer" href='https://github.com/Parvez-Rahman100'><Icon style={{ fontSize: '40px' }} icon="icon-park:github" /></a>
+            <a className=' mx-5 mt-3' target='_blank' rel="noreferrer" href='https://www.linkedin.com/in/parvez-miah-945910229/'><Icon style={{ fontSize: '40px' }} icon="logos:linkedin" /></a>
+            <a className=' mx-5 mt-3' target='_blank' rel="noreferrer" href='https://www.facebook.com/groups/3128511790748537/'><Icon style={{ fontSize: '40px' }} icon="icon-park:facebook" /></a>
+          </div>
+        </div>
+
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <div className="card-body">
+            <form action="https://formsubmit.co/iamparvezrahman@gmail.com" method='POST'>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Your Name</span>
+                </label>
+                <input type="text" name='name' placeholder="Your Name" className="input input-bordered" required />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Your Message</span>
+                </label>
+                <textarea type="text" name='message' placeholder="Your Message" className="input input-bordered" required />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input type="text" name='email' placeholder="Email" className="input input-bordered" required />
+              </div>
+              <div className="form-control mt-6">
+                <button type='submit' className="btn btn-primary">Send Message </button>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
-    
-    <div className="card  flex-shrink-0 w-full max-w-sm shadow-2xl mx-52 bg-base-100 text-center">
-      <div className="card-body">
-      <form action="https://formsubmit.co/iamparvezrahman@gmail.com" method='POST'>
-      <div className="form-control">
-          <label className="label">
-            <span className="label-text">Your Name</span>
-          </label>
-          <input type="text" name='name' placeholder="Your Name" className="input input-bordered" required />
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Your Message</span>
-          </label>
-          <textarea type="text" name='message' placeholder="Your Message" className="input input-bordered" required />
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Your Email</span>
-          </label>
-          <input type="text" name='email' placeholder="Email" className="input input-bordered" required/>
-        </div>
-        <div className="form-control mt-6">
-          <button type='submit' className="btn btn-primary">Send Message </button>
-        </div>
-      </form>
-      </div>
-    </div>
-  </div>
-</div>
-        </div>
-    );
+  );
 };
 
 export default Contact;
--- src/Components/Shared/Footer.js
+++ src/Components/Shared/Footer.js
@@ -3,15 +3,15 @@ import { Icon } from '@iconify/react';
 
 const Footer = () => {
     return (
-        <div style={{backgroundColor:'rgba(203, 203, 203, 1)'}} className='flex justify-between items-center'>
+        <div className=' bg-accent flex justify-between items-center'>
             <div>
                 <p className=' text-center tracking-widest text-xl font-semibold'>Alumni Association</p>
                 <div className='flex lg:justify-start justify-center items-center my-4'>
-                <a target='_blank' rel="noreferrer" href='https://github.com/Parvez-Rahman100'><Icon className=' mx-7 w-10  h-6 ' icon="fa:github" /></a>
-                <a target='_blank' rel="noreferrer" href='https://www.linkedin.com/in/parvez-miah-945910229/'><Icon className=' mx-7 w-10  h-6' icon="line-md:linkedin" /></a>
-                <a target='_blank' rel="noreferrer" href='https://www.facebook.com/groups/3128511790748537'><Icon className=' mx-7 w-10  h-6' icon="akar-icons:facebook-fill" /></a>
-                <a target='_blank' rel="noreferrer" href='https://www.instagram.com/i_amparvezrahman/'><Icon className=' mx-7 w-10  h-6' icon="akar-icons:instagram-fill" /></a>
-            </div>
+                    <a target='_blank' rel="noreferrer" href='https://github.com/Parvez-Rahman100'><Icon className=' mx-7 w-10  h-6 ' icon="fa:github" /></a>
+                    <a target='_blank' rel="noreferrer" href='https://www.linkedin.com/in/parvez-miah-945910229/'><Icon className=' mx-7 w-10  h-6' icon="line-md:linkedin" /></a>
+                    <a target='_blank' rel="noreferrer" href='https://www.facebook.com/groups/3128511790748537'><Icon className=' mx-7 w-10  h-6' icon="akar-icons:facebook-fill" /></a>
+                    <a target='_blank' rel="noreferrer" href='https://www.instagram.com/i_amparvezrahman/'><Icon className=' mx-7 w-10  h-6' icon="akar-icons:instagram-fill" /></a>
+                </div>
             </div>
             <div>
                 <p> &copy; Copyright 2022-2023, Alumni Association , All Rights Reserved</p>
